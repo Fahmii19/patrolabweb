85bb6d18c1f515ae9540d7659672280b62528e21
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2023-12-02 06:27:00 +0700

    Fixed asset controller and asset edit interface

--- resources/views/super-admin/aset/edit.blade.php
+++ resources/views/super-admin/aset/edit.blade.php
@@ -29,44 +29,22 @@
                 <div class="row row-cols-1 row-cols-lg-2">
                     <div class="col">
                         <div class="mb-3">
-                            <label for="code" class="form-label">code Aset <span class="text-danger">*</span></label>
+                            <label for="code" class="form-label">Kode Aset <span class="text-danger">*</span></label>
                             <input type="text" class="form-control @error('code') is-invalid @enderror" name="code" id="code" value="{{ old('code', $aset->code) }}" placeholder="Masukkan code aset">
                             @error('code')
                             <span class="text-danger d-block">{{ $message }}</span>
                             @enderror
                         </div>
-
                         <div class="mb-3">
-                            <label for="name" class="form-label">name Aset <span class="text-danger">*</span></label>
+                            <label for="name" class="form-label">Nama Aset <span class="text-danger">*</span></label>
                             <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{ old('name', $aset->name) }}" placeholder="Masukkan name aset">
                             @error('name')
                             <span class="text-danger d-block">{{ $message }}</span>
                             @enderror
                         </div>
-
                         <div class="mb-3">
-                            <label for="status" class="form-label">Status <span class="text-danger">*</span></label>
-                            <select class="form-control @error('status') is-invalid @enderror" name="status" id="status">
-                                <option value="" disabled>Pilih status aset</option>
-                                <option value="ACTIVED" {{ old('status', $aset->status) == 'ACTIVED' ? 'selected' : '' }}>ACTIVED</option>
-                                <option value="INACTIVED" {{ old('status', $aset->status) == 'INACTIVED' ? 'selected' : '' }}>INACTIVED</option>
-                            </select>
-                            @error('status')
-                            <span class="text-danger d-block">{{ $message }}</span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="short_desc" class="form-label">Deskripsi Singkat</label>
-                            <textarea class="form-control @error('short_desc') is-invalid @enderror" name="short_desc" id="short_desc" placeholder="Masukkan deskripsi singkat">{{ old('short_desc', $aset->short_desc) }}</textarea>
-                            @error('short_desc')
-                            <span class="text-danger d-block">{{ $message }}</span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="asset_master_type" class="form-label">Tipe Aset</label>
-                            <select class="form-control @error('asset_master_type') is-invalid @enderror" name="asset_master_type" id="asset_master_type">
+                            <label for="asset_master_type" class="form-label">Tipe Aset <span class="text-danger">*</span></label>
+                            <select class="form-select @error('asset_master_type') is-invalid @enderror" name="asset_master_type" id="asset_master_type">
                                 <option value="" disabled selected>Pilih tipe aset</option>
                                 <option value="PATROL" {{ old('asset_master_type', $aset->asset_master_type) == 'PATROL' ? 'selected' : '' }}>Patrol</option>
                                 <option value="CLIENT" {{ old('asset_master_type', $aset->asset_master_type) == 'CLIENT' ? 'selected' : '' }}>Client</option>
@@ -75,7 +53,13 @@
                             <span class="text-danger d-block">{{ $message }}</span>
                             @enderror
                         </div>
-
+                        <div class="mb-3">
+                            <label for="short_desc" class="form-label">Deskripsi Singkat</label>
+                            <textarea class="form-control @error('short_desc') is-invalid @enderror" name="short_desc" id="short_desc" placeholder="Masukkan deskripsi singkat">{{ old('short_desc', $aset->short_desc) }}</textarea>
+                            @error('short_desc')
+                            <span class="text-danger d-block">{{ $message }}</span>
+                            @enderror
+                        </div>
                         <div class="mb-3">
                             <label for="image" class="form-label">Gambar Aset</label>
                             <input type="file" class="form-control @error('image') is-invalid @enderror" name="image" id="image">
@@ -85,8 +69,11 @@
                             <!-- Tampilkan gambar saat ini jika ada, jika tidak, tampilkan gambar default -->
                             <img src="{{ $aset->image ? asset('gambar/aset/' . $aset->image) : asset('gambar/no-image.png') }}" width="100" height="100" class="img-thumbnail mt-2">
                         </div>
-
-
+                        <div class="mb-3 align-middle">
+                            <input type="checkbox" class="form-check-input fs-5 mt-0 me-2 @error('status') is-invalid @enderror" name="status" id="checkPointStatus" value="ACTIVED" {{ $aset->status == 'ACTIVED' ? 'checked' : '' }}>
+                            <label for="checkPointStatus" class="align-middle mb-0">Aktif</label>
+                            @error('status') <span class="text-danger d-block">{{$message}}</span> @enderror
+                        </div>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-success">Simpan</button>
