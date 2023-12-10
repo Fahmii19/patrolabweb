c93782a683718869061366e73f9041c9b673ab3c
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2023-12-10 17:29:34 +0700

    added interface for checkpoint report log

--- resources/views/super-admin/checkpoint-report/index.blade.php
+++ resources/views/super-admin/checkpoint-report/index.blade.php
@@ -9,7 +9,7 @@
             <div class="col-6">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}"> <i data-feather="home"></i></a></li>
-                    <li class="breadcrumb-item">Checkpoint Report</li>
+                    <li class="breadcrumb-item">Reporting</li>
                     <li class="breadcrumb-item">{{ $title }}</li>
                 </ol>
             </div>
@@ -21,7 +21,7 @@
     <div class="card">
         <div class="card-body">
             <div class="d-flex mb-3 justify-content-end">
-                <a href="{{route('check-point.create')}}" class="btn btn-success">Tambah CheckPoint Report</a>
+                <a href="{{route('self-patrol.create')}}" class="btn btn-success">Tambah CheckPoint Report</a>
             </div>
             <table id="mytable" class="display" style="width:100%">
                 <thead>
@@ -29,32 +29,14 @@
                         <th style="max-width: 40px;">No</th>
                         <th>Nama CheckPoint</th>
                         <th>Lokasi CheckPoint</th>
-                        <th>Lattitude</th>
+                        <th>Nama Guard</th>
+                        <th>Shift</th>
+                        <th>Tanggal</th>
                         <th>Mulai</th>
                         <th>Selesai</th>
-                        <th>Tanggal</th>
                     </tr>
                 </thead>
             </table>
-        </div>
-    </div>
-</div>
-
-<!-- Modal Body -->
-<!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-<div class="modal fade" id="qr_modal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
-        <div class="modal-content rounded-0">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTitleId">QR Code</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div id="qr_image"></div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
         </div>
     </div>
 </div>
@@ -68,34 +50,31 @@
         columns: [{
                 data: 'DT_RowIndex',
                 name: 'No'
-            },
-            {
-                data: 'checkpoint_name_log',
+            }, {
+                data: 'checkpoint_name',
                 name: 'Nama CheckPoint'
-            },
-            {
-                data: 'checkpoint_location_log',
+            }, {
+                data: 'checkpoint_loc',
                 name: 'Lokasi CheckPoint'
-            },
-            {
-                data: 'checkpoint_location_long_lat_log',
-                name: 'Lattitude'
-            },
-            {
-                data: 'shift_start_time_log',
-                name: 'Mulai'
-            },
-            {
-                data: 'shift_end_time_log',
-                name: 'Selesai'
-            },
-            {
-                data: 'business_date',
-                name: 'Tanggal'
-            },
+            }, {
+                data: 'guard',
+                name: 'Nama Guard'
+            }, {
+                data: 'shift',
+                name: 'Nama Shift'
+            }, {
+                data: 'patrol_date',
+                name: 'Tanggal Patrol'
+            }, {
+                data: 'start_time',
+                name: 'Jam Mulai'
+            }, {
+                data: 'finish_time',
+                name: 'Jam Selesai'
+            }
         ]
     });
-    active_menu("#menu-checkpoint-report", "#sub-list-checkpoint-report")
+    active_menu("#menu-report", "#sub-list-checkpoint-report")
 </script>
 
 @endpush
