f5c285f47134367eb95bac9c08b4b3d9da2eac11
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2023-12-27 21:01:37 +0700

    fixed round crud

--- resources/views/super-admin/round/create.blade.php
+++ resources/views/super-admin/round/create.blade.php
@@ -9,121 +9,82 @@
             <li class="breadcrumb-item">{{ $title }}</li>
         @endslot
     @endcomponent
-<!-- Container-fluid starts-->
-<div class="container-fluid">
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex justify-content-end">
-                <button type="buton" onclick="window.location.href='{{ route('round.index') }}'" class="btn btn-warning text-dark">
-                    << Kembali
-                </button>
-            </div>
-            <form action="{{route('round.store')}}" method="POST">
-                @csrf
-                <div class="row row-cols-2">
-                    <div class="col">
-                        <div class="mb-3">
-                            <label for="selectWilayah" class="form-label">Nama Wilayah <span class="text-danger">*</span></label>
-                            <select class="form-select @error('id_wilayah') is-invalid @enderror" name="id_wilayah" onchange="get_project(this.value)" id="selectWilayah">
-                                <option value="" selected disabled>--Pilih--</option>
-                                @foreach ($wilayah as $item)
-                                    <option value="{{ $item->id }}" {{ old('id_wilayah') == $item->id ? 'selected' : '' }}>{{ $item->name }}
-                                </option>
-                                @endforeach
-                            </select>
-                            @error('id_wilayah') <span class="text-danger d-block">{{ $message }}</span> @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="selectProject" class="form-label">Nama Project <span class="text-danger">*</span></label>
-                            <select class="form-select @error('id_project') is-invalid @enderror" name="id_project" onchange="get_area(this.value)" id="selectProject">
-                                <option value="" selected disabled>--Pilih--</option>
-                            </select>
-                            <span class="text-danger d-block" id="project-alert"></span>
-                            @error('id_project') <span class="text-danger d-block">{{ $message }}</span> @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="selectArea" class="form-label">Nama Area <span class="text-danger">*</span></label>
-                            <select class="form-select @error('id_area') is-invalid @enderror" name="id_area" id="selectArea">
-                                <option value="" selected disabled>--Pilih--</option>
-                            </select>
-                            <span class="text-danger d-block" id="area-alert"></span>
-                            @error('id_area') <span class="text-danger d-block">{{ $message }}</span> @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Nama Rute <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control @error('nama') is-invalid @enderror" name="rute" id="name" value="{{old('nama')}}" placeholder="Masukkan Nama Rute">
-                            @error('nama') <span class="text-danger d-block">{{$message}}</span> @enderror
-                        </div>
-                        <div class="mb-3">
-                            <button class="btn btn-primary">Simpan</button>
+    <!-- Container-fluid starts-->
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex justify-content-end">
+                    <button type="buton" onclick="window.location.href='{{ route('round.index') }}'" class="btn btn-warning text-dark">
+                        << Kembali
+                    </button>
+                </div>
+                <form action="{{route('round.store')}}" method="POST">
+                    @csrf
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="selectArea" class="form-label">Nama Area <span class="text-danger">*</span></label>
+                                <select class="form-select @error('area_id') is-invalid @enderror" name="area_id" onchange="get_patrol_area(this.value)" id="selectArea" required>
+                                    <option value="" selected disabled>--Pilih--</option>
+                                    @foreach ($area as $item)
+                                        <option value="{{ $item->id }}" {{ old('area_id') == $item->id ? 'selected' : '' }}>{{ $item->name }}
+                                    </option>
+                                    @endforeach
+                                </select>
+                                @error('area_id') <span class="text-danger d-block">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="selectPatrolArea" class="form-label">Nama Patrol Area <span class="text-danger">*</span></label>
+                                <select class="form-select @error('patrol_area_id') is-invalid @enderror" name="patrol_area_id" id="selectPatrolArea" required>
+                                    <option value="" selected disabled>--Pilih--</option>
+                                </select>
+                                <span class="text-danger d-block" id="patrol-area-alert"></span>
+                                @error('patrol_area_id') <span class="text-danger d-block">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Nama Rute <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{old('name')}}" placeholder="Masukkan nama rute" required>
+                                @error('name') <span class="text-danger d-block">{{$message}}</span> @enderror
+                            </div>
+                            <div class="mb-3 text-end">
+                                <button class="btn btn-success">Simpan</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-<!-- Container-fluid Ends-->
+    <!-- Container-fluid Ends-->
+@endsection
 
 @push('js')
-<script>
-    function get_project(id_wilayah) {
-        let project_base = $('#selectProject')
-        let project_alert = $('#project-alert')
-        $.ajax({
-            url: "{{ url('/super-admin/project-by-wilayah-select') }}/" + id_wilayah,
-            method: 'get',
-            data: {
-                id_project: "{{ old('id_project') }}"
-            },
-            //menghapus checkbox sebelumnya jika di select form lain
-            beforeSend: function() {
-                project_alert.removeClass('text-danger').addClass('text-black').text('Mengambil data project')
-            },
+    <script>
+        function get_patrol_area(area_id) {
+            let patrol_area_base = $('#selectPatrolArea')
+            let patrol_area_alert = $('#patrol-area-alert')
+            $.ajax({
+                url: "{{ url('/super-admin/patrol-area-by-area') }}/" + area_id,
+                method: 'get',
+                data: {
+                    area_id: "{{ old('area_id') }}"
+                },
+                beforeSend: function() {
+                    patrol_area_alert.removeClass('text-danger').addClass('text-black').text('Mengambil data patrol area')
+                },
 
-            success: function(response) {
-                let data = response.data
-                project_base.html(data)
-                project_alert.text('')
-            },
-            error: function(response) {
-                project_base.html('<option value="" selected disabled>--Pilih--</option>')
-                project_alert.removeClass('text-black').addClass('text-danger').text('Tidak ada data project di wilayah ini')
-                //console.log(response)
-            }
-        })
-    }
+                success: function(response) {
+                    let data = response.data
+                    patrol_area_base.html(data)
+                    patrol_area_alert.text('')
+                },
+                error: function(response) {
+                    patrol_area_base.html('<option value="" selected disabled>--Pilih--</option>')
+                    patrol_area_alert.removeClass('text-black').addClass('text-danger').text('Tidak ada data patrol area di area ini')
+                }
+            })
+        }
 
-    function get_area(id_project) {
-        let area_base = $('#selectArea')
-        //let project_item = $('#project_item').clone().removeAttr('id')
-        let area_alert = $('#area-alert')
-        $.ajax({
-            url: "{{ url('/super-admin/area-by-project') }}/" + id_project,
-            method: 'get',
-            data: {
-                id_area: "{{ old('id_area') }}"
-            },
-            //menghapus checkbox sebelumnya jika di select form lain
-            beforeSend: function() {
-                area_alert.removeClass('text-danger').addClass('text-black').text('Mengambil data area')
-            },
-
-            success: function(response) {
-                let data = response.data
-                area_base.html(data)
-                area_alert.text('')
-            },
-            error: function(response) {
-                area_base.html('<option value="" selected disabled>--Pilih--</option>')
-                area_alert.removeClass('text-black').addClass('text-danger').text('Tidak ada data area di project ini')
-            }
-        })
-    }
-
-    
-    active_menu("#menu-round", "#sub-round-create")
-</script>
-
+        active_menu("#menu-round", "#sub-round-create")
+    </script>
 @endpush
-@endsection
