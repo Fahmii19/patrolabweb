20644c41bb9e8a6f6c0505e56a4ed929a0a8eeb0
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2023-12-27 20:20:56 +0700

    added guard management pleton patrol area

---
+++ app/Http/Controllers/PletonPatrolAreaController.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PatrolArea;
+use App\Models\Pleton;
+use Exception;
+use Throwable;
+use App\Models\PletonPatrolArea;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
+
+class PletonPatrolAreaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['title'] = "Daftar Pleton Patrol Area";
+        return view('super-admin.pleton-patrol.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data['title'] = 'Tambah Pleton Patrol';
+        $data['patrol_area'] = PatrolArea::all();
+        $selectedPletonIds = PletonPatrolArea::pluck('pleton_id')->toArray();
+        $data['pleton'] = Pleton::whereNotIn('id', $selectedPletonIds)->get();
+
+        return view('super-admin.pleton-patrol.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $validator = Validator::make($request->all(), [
+                'pleton_id' => 'required|numeric',
+                'patrol_area_id' => 'required|numeric',
+            ]);
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator->errors())->withInput($request->all());
+            }
+
+            $data = $validator->validated();
+            $data['created_at'] = now();
+            $data['updated_at'] = null;
+
+            PletonPatrolArea::create($data);
+            DB::commit();
+
+            return redirect()->route('pleton-patrol-area.index')->with('success', 'Pleton patrol area berhasil ditambahkan');
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::debug('PletonPatrolAreaController store() error:' . $e->getMessage());
+            return redirect()->back()->with('error', 'Pleton patrol area gagal ditambahkan: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data['title'] = 'Edit Pleton Patrol Area';
+        $pletonPatrol = PletonPatrolArea::find($id);
+
+        if(!$pletonPatrol){
+            return redirect()->back()->with('error', 'Pleton patrol area tidak ditemukan.');
+        }
+
+        $data['pleton_patrol'] = $pletonPatrol;
+        $data['patrol_area'] = PatrolArea::all();
+
+        $selectedPletonIds = PletonPatrolArea::pluck('pleton_id')->toArray();
+        $pletonNotSelected = Pleton::whereNotIn('id', $selectedPletonIds)->get();
+        $pletonSelected = Pleton::where('id', $pletonPatrol->pleton_id)->get();
+
+        $data['pleton'] = $pletonNotSelected->merge($pletonSelected);
+        return view('super-admin.pleton-patrol.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            DB::beginTransaction();
+
+            $pletonPatrol = PletonPatrolArea::find($id);
+
+            if(!$pletonPatrol){
+                return redirect()->back()->with('error', 'Pleton patrol area tidak ditemukan.');
+            }
+
+            $validator = Validator::make($request->all(), [
+                'pleton_id' => 'required|numeric',
+                'patrol_area_id' => 'required|numeric',
+            ]);
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator->errors())->withInput($request->all());
+            }
+
+            $data = $validator->validated();
+            $data['created_at'] = $pletonPatrol->created_at;
+            $data['updated_at'] = now();
+
+            $pletonPatrol->update($data);
+            DB::commit();
+
+            return redirect()->route('pleton-patrol-area.index')->with('success', 'Pleton patrol area berhasil diperbarui');
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::debug('PletonPatrolAreaController update() error:' . $e->getMessage());
+            return redirect()->back()->with('error', 'Pleton patrol area gagal diperbarui: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            DB::beginTransaction();
+            $pletonPatrol = PletonPatrolArea::find($id);
+            if (!$pletonPatrol) {
+                return redirect()->back()->with('error', 'Pleton patrol area tidak ditemukan.');
+            }
+
+            $pletonPatrol->delete();
+            DB::commit();
+
+            return redirect()->route('pleton-patrol-area.index')->with('success', 'Pleton patrol area berhasil dihapus');
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::debug('PletonPatrolAreaController destroy() error:' . $e->getMessage());
+            return redirect()->back()->with('error', 'Pleton patrol area gagal dihapus: ' . $e->getMessage());
+        }
+    }
+
+    public function datatable()
+    {
+        $data = PletonPatrolArea::with('pleton', 'patrol_area')->get();
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->escapeColumns([])
+            ->addColumn('patrol_area', '{{$pleton["name"]}}')
+            ->addColumn('pleton', '{{$patrol_area["name"]}}')
+            ->addColumn('action', function (PletonPatrolArea $data) {
+                return [
+                    'editurl' => route('pleton-patrol-area.edit', $data->id),
+                    'deleteurl' => route('pleton-patrol-area.destroy', $data->id),
+                ];
+            })
+        ->toJson();
+    }
+}
---
+++ resources/views/super-admin/pleton-patrol/index.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.admin')
+@section('content')
+    @component('components.dashboard.headpage')
+        @slot('title')
+            {{ $title }}
+        @endslot
+        @slot('bread')
+            <li class="breadcrumb-item">Guard Management</li>
+            <li class="breadcrumb-item">{{ $title }}</li>
+        @endslot
+    @endcomponent
+
+    <!-- Container-fluid starts-->
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex mb-3 justify-content-end">
+                    <a href="{{ route('pleton-patrol-area.create') }}" class="btn btn-success">Tambah Pleton Patrol</a>
+                </div>
+                <div class="table-responsive">
+                    <table id="mytable" class="display" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th style="max-width: 40px;">No</th>
+                                <th class="text-nowrap">Patrol Area</th>
+                                <th class="text-nowrap">Nama Pleton</th>
+                                <th class="text-nowrap">Aksi</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="actionbase" class="d-none">
+        <div class="d-flex">
+            <form method="post">
+                @csrf
+                @method('delete')
+                <a id="edit" class="btn btn-warning me-2 my-2 text-dark">Edit</a>
+                <button onclick="hapus_data(event)" class="btn btn-danger me-2" type="button">Hapus</button>
+            </form>
+        </div>
+    </div>
+    <!-- Container-fluid Ends-->
+@endsection
+
+@push('js')
+    <script>
+        $(document).ready(function() {
+            $('#mytable').DataTable({
+                scrollX: true,
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('pleton-patrol.datatable') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false,
+                    },
+                    {
+                        data: 'patrol_area',
+                        name: 'Nama Patrol Area',
+                    },
+                    {
+                        data: 'pleton',
+                        name: 'Nama Pleton',
+                    },
+                    {
+                        name: "Action",
+                        orderable: false,
+                        searchable: false,
+                        render: function(data, type, row) {
+                            let html = $('#actionbase').clone();
+                            html = html.find('.d-flex');
+                            html.find('#edit').attr('href', row.action.editurl);
+                            let form = html.find('form').attr('action', row.action.deleteurl)
+                                .attr('id', 'delete_form' + row.id);
+                            form.find('button').attr('form-id', '#delete_form' + row.id);
+                            return html.html();
+                        }
+                    }
+                ]
+            });
+        });
+
+        active_menu("#menu-guard", "#sub-pleton-patrol")
+    </script>
+@endpush
