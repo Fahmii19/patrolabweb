edcba38ba306d03372210a0e5b723560d079f48d
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-01-11 20:16:50 +0700

    added new function upload and load image with API

--- app/Helper/Helpers.php
+++ app/Helper/Helpers.php
@@ -25,11 +25,61 @@ if(!function_exists('insert_audit_log')){
 
 if(!function_exists('redis_reset_api')){
     function redis_reset_api($endpoint){
-        $url = 'http://api-sit.patrol.adonara.co.id/redis-reset/' . $endpoint;
+        $url = env('API_URL') . 'redis-reset/' . $endpoint;
 
-        Http::withHeaders([
+        $response = Http::withHeaders([
             'Accept' => '*/*',
-            'Authorization' => 'Basic cGF0cm9sOnBhdHJvbCMxMjM0',
+            'Authorization' => env('BASIC_AUTH'),
         ])->delete($url);
+
+        return $response->body();
+    }
+}
+
+if(!function_exists('upload_image_api')){
+    function upload_image_api($file, $name){
+        $url = env('API_URL') . 'files/web/image/upload';
+
+        $response = Http::attach('image', $file, $name)
+        ->withHeaders([
+            'Accept' => '*/*',
+            'Authorization' => env('BASIC_AUTH'),
+        ])->post($url);
+
+        return $response->body();
+    }
+}
+
+
+if(!function_exists('get_image_api')){
+    function get_image_api($filename){
+        $url = env('API_URL') . 'files/web/image/get/' . $filename;
+
+        $response = Http::withHeaders([
+            'Accept' => '*/*',
+            'Authorization' => env('BASIC_AUTH'),
+        ])->get($url);
+
+        if ($response->successful()) {
+            // Return raw image content without additional headers
+            return $response->body();
+        } else {
+            // Handle error case (e.g., image not found)
+            return null;
+        }
+    }
+}
+
+
+if(!function_exists('check_img_path')){
+    function check_img_path($filename){
+        $imgContent = get_image_api($filename);
+
+        if ($imgContent !== null) {
+            $base64Image = base64_encode($imgContent);
+            return "data:image/*;base64," . $base64Image;
+        }
+
+        return asset('gambar/no-image.png'); // Gambar default
     }
 }
